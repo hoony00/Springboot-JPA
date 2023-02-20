34175bf3b8a3cd77da74256fc0e185ce41268859
hoon <nasi3611@naver.com>
2023-02-20 23:25:59 +0900

    feat(prj) 테스트 컨트롤러 추가
    feat(prj) 테스트 컨트롤러 추가

--- src/main/java/com/example/blog/controller/DummyControllerTest.java
+++ src/main/java/com/example/blog/controller/DummyControllerTest.java
@@ -8,7 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DummyControllerTest {
     @PostMapping("/dummy/join")
     public String join(@RequestParam String username, String password, String email){
-       return username;
+        System.out.println("username : " + username);
+       return "회원가입 완료";
+    }
 
+    @PostMapping("/dummy/join")
+    public String joi2n(@RequestParam String username, String password, String email){
+        System.out.println("username : " + username);
+        return "회원가입 완료";
     }
 }
